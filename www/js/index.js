f647b7edc3b89885b6875f0f62e2eefc7854fd20
VVV <vvv@VVVs-Mac.local>
2019-01-13 19:14:22 -0800

    updated ios

--- www/js/index.js
+++ www/js/index.js
@@ -44,6 +44,15 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
+        FCMPlugin.onNotification(function(data){
+            if(data.wasTapped){
+              //Notification was received on device tray and tapped by the user.
+              alert( JSON.stringify(data) );
+            }else{
+              //Notification was received in foreground. Maybe the user needs to be notified.
+              alert( JSON.stringify(data) );
+            }
+        });
         console.log('Received Event: ' + id);
     }
 };
